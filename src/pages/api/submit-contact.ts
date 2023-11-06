efd73976f5eda1c9bc8c80484cf3ee66d3811ac4
JAL <javier.angulo.luna@gmail.com>
2023-11-06 18:41:13 +0100

    notion client and moved to api

--- src/pages/api/submit-contact.ts
+++ src/pages/api/submit-contact.ts
@@ -1,27 +1,22 @@
 import type { APIRoute } from "astro";
+import { Client } from "@notionhq/client";
 
 export const prerender = true;
 
 const NOTION_API_KEY = import.meta.env.NOTION_API_KEY;
-const NOTION_COOKIE = import.meta.env.NOTION_COOKIE;
+const NOTION_DATABASE_ID = import.meta.env.NOTION_DATABASE_ID;
+const NOTION_USER_ID = import.meta.env.NOTION_USER_ID;
 
-const sendForm = (fields: any): any => {
-    var myHeaders = new Headers();
-    myHeaders.append("notion-version", "2022-06-28");
-    myHeaders.append("Content-Type", "application/json");
-    myHeaders.append(
-        "Authorization",
-        `Bearer  ${NOTION_API_KEY}`
-    );
-    myHeaders.append(
-        "Cookie",
-        NOTION_COOKIE
-    );
+const notion = new Client({
+    auth: NOTION_API_KEY,
+});
 
-    var raw = JSON.stringify({
+const sendForm = async (fields: any) => {
+
+    return notion.pages.create({
         parent: {
             type: "database_id",
-            database_id: "2a161a0f40b947709bd38855e99069a8",
+            database_id: NOTION_DATABASE_ID,
         },
         properties: {
             Name: {
@@ -46,16 +41,17 @@ const sendForm = (fields: any): any => {
                 type: "rich_text",
                 rich_text: [{ text: { content: fields.message.value } }],
             },
+            Person: {
+                type: "people",
+                people: [
+                    {
+                        object: "user",
+                        id: NOTION_USER_ID
+                    }
+                ]
+            }
         },
-    });
-
-    var requestOptions = {
-        method: "POST",
-        headers: myHeaders,
-        body: raw,
-    };
-
-    return fetch("https://api.notion.com/v1/pages", requestOptions)
+    })
 };
 
 
@@ -65,17 +61,13 @@ export const POST: APIRoute = async ({ request }) => {
 
     try {
         const data = await request.json();
-
-        const notion = await sendForm(data)
-        const jsonNotion = await notion.json()
-        return new Response(JSON.stringify({notion: jsonNotion})
+        const notionResponse = await sendForm(data)
+        return new Response(JSON.stringify({notionId: notionResponse.id})
         )
 
     } catch (error) {
-
-        return new Response(JSON.stringify({error}))
+        console.error(error)
+        return new Response(JSON.stringify({error: 'error'}))
     }
-
-
 
 }
